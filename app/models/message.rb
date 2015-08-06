288d5dfd107594916a2a1837a97d551da607986f
David West <thedavidwest@gmail.com>
2015-08-06 11:59:59 -0500

    Fixed wordwrap

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ActiveRecord::Base
   before_save :created_at
 
   def timestamp
-    created_at_field = strftime("%a, %B %d, %I:%M %p")
+    created_at_field = Time.strftime("%a, %B %d, %I:%M %p")
   end
   def change
   end
