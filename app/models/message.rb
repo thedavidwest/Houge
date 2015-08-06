34964b29e536f96d154b4f36c295bd2f45783101
David West <thedavidwest@gmail.com>
2015-08-06 13:26:51 -0500

    started responsiveness

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ActiveRecord::Base
   before_save :created_at
 
   def timestamp
-    created_at_field = Time.strftime("%a, %B %d, %I:%M %p")
+    created_at_field = Time.in_time_zone.strftime("%a, %B %d, %I:%M %p")
   end
   def change
   end
