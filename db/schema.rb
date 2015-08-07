f33c0c38f5a660595560bce4fc13c76dc3592b91
David West <thedavidwest@gmail.com>
2015-08-07 10:10:50 -0500

    Added user_id to messages and updated sass

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806192834) do
+ActiveRecord::Schema.define(version: 20150807122323) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
