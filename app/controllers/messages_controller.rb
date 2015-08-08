e6687a6396defe976a7055f8c189edffa6fce27f
David West <thedavidwest@gmail.com>
2015-08-07 21:57:50 -0500

    added usernames, edit and delete functions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,12 +10,33 @@ class MessagesController < ApplicationController
   def new
     @message = current_user.messages.build
   end
+  def show
+  end
   def create
-    @message = Message.new(message_params)
+    @message = current_user.messages.build(message_params)
     if @message.save
       redirect_to '/messages'
     else
       render 'new'
+  end
+
+  def update
+    respond_to do |format|
+      if @message.update(message_params)
+        format.html { redirect_to '/messages', notice: 'Message was successfully updated.' }
+        format.json { render :show, status: :ok, location: @messages }
+      else
+        format.html { render :edit }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  def destroy
+    @message.destroy
+    respond_to do |format|
+      format.html { redirect_to messages_url, notice: 'Message was successfully deleted.' }
+      format.json { head :no_content }
+    end
   end
 end
   private
@@ -28,6 +49,6 @@ end
     redirect_to messages_path, notice: "Unable to locate page." if @message.nil?
   end
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content, :username)
   end
 end
